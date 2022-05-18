e12e9aff418b264c7881dfd9bcc9cd88bd1b73f7
EAM0719 <60077797+EAM0719@users.noreply.github.com>
2022-05-18 10:35:52 -0400

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,3 +0,0 @@
-console.log("The index works!"); // This is just here for me to check if the index works:
-
-window.location = "https://syneural.github.io/home";  // This is to redirect to the homepage of our website.
